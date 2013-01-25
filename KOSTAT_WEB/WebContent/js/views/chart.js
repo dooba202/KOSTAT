3daf3c897ea3b528cf643000892ac0f38a829eec
Dongkyun Yoo <dkstyle@gmail.com>
2013-01-25 15:01:24 +0900

    chart label update

--- KOSTAT_WEB/WebContent/js/views/chart.js
+++ KOSTAT_WEB/WebContent/js/views/chart.js
@@ -43,6 +43,12 @@ function($,Backbone, _, Mustache, Logger) {
 		},
 		
 		render : function(data, unit) {
+			Highcharts.setOptions({
+				lang: {
+	 				rangeSelectorZoom: "확대"
+				}
+			});
+			
  			this.chart = new Highcharts.StockChart({
  				credits: false,
  				
@@ -58,14 +64,16 @@ function($,Backbone, _, Mustache, Logger) {
  		    	xAxis: {
  		    		dateTimeLabelFormats: 
  					{ 
- 						second: '%Y-%m-%d<br/>%H:%M:%S',
- 						minute: '%Y-%m-%d<br/>%H:%M',
- 						hour: '%Y-%m-%d<br/>%H:%M',
- 						day: '%Y<br/>%m-%d',
- 						week: '%Y<br/>%m-%d',
- 						month: '%Y-%m',
- 						year: '%Y'
- 					}
+ 						second: '%Y년%m월%d일<br/>%H:%M:%S',
+ 						minute: '%Y년%m월%d일<br/>%H:%M',
+ 						hour: '%Y년%m월%d일<br/>%H:%M',
+ 						day: '%Y년<br/>%m월%d일',
+ 						week: '%Y년<br/>%m월%d일',
+ 						month: '%Y년%m월',
+ 						year: '%Y년'
+ 					},
+ 					minTickInterval: 24 * 3600 * 1000 * 30, //To keep one month interval at least
+ 					minRange: 3,
  		    	},
  		    	
  			    yAxis: {
@@ -115,21 +123,24 @@ function($,Backbone, _, Mustache, Logger) {
  			    },	
  			    
  			    rangeSelector: { 
- 			    	selected: 0, 
+ 			    	selected: 2, 
  			    	buttons: [
- 				    	/*
  				    	{
  							type: 'month',
  							count: 3,
  							text: '3m'
- 						},*/ {
+ 						}, 
+ 						{
  							type: 'month',
  							count: 6,
  							text: '6m'
- 						}, {
+ 						}, 
+ 						/*
+ 						{
  							type: 'ytd',
  							text: 'YTD'
- 						}, {
+ 						},*/ 
+ 						{
  							type: 'year',
  							count: 1,
  							text: '1y'
@@ -138,7 +149,7 @@ function($,Backbone, _, Mustache, Logger) {
  							text: 'All'
  						}
  					],
- 					inputDateFormat: '%Y-%m',
+ 					inputDateFormat: '%Y년%m월',
  					inputEditDateFormat: '%Y-%m'
  			    },
  			    /*
@@ -149,7 +160,7 @@ function($,Backbone, _, Mustache, Logger) {
  			    },
  				*/			    
  			    tooltip: {
- 			    	xDateFormat: '%Y-%m',
+ 			    	xDateFormat: '%Y년%m월',
  			    	headerFormat: '<span style="font-size: 10px">{point.key}</span><br/>',
  			    	pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}' + unit +'</b><br/>', //({point.change}%)
  			    	//valueDecimals: 2
